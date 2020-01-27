5fd2d25123d4953c11dea8bc00ee0893ede38cdb
Kohleth Chia <KChia@bunnings.com.au>
2020-01-27 22:59:32 +1100

    added hctsa features

--- modelability/experiments/exp5/utils.R
+++ modelability/experiments/exp5/utils.R
@@ -29,8 +29,41 @@ mycroston=function (y, h = 10, alpha = 0.1, x = y)
   return(structure(out, class = "forecast"))
 }
 
-myfeature=function(.ts_list,fn,packages=NULL){
-  foreach(ts=ichunk(.ts_list,chunkSize=1000),.final=function(x)unlist(x,use.names=F),.packages=packages)%dopar%{
+myfeature=function(.ts_list,fn,...){
+  foreach(ts=ichunk(.ts_list,chunkSize=1000),.final=function(x)unlist(x,use.names=F),...)%dopar%{
     unlist(lapply(ts,fn),use.names=F)
   }
+}
+
+
+## a port of https://github.com/benfulcher/hctsa/blob/34a92628bd13dec09ea137f0d7040c4be6176400/Operations/SB_CoarseGrain.m
+## method = 'updown' only
+sb_coarsegrain=function(y,numGroups=2){
+  y=diff(y)
+  th=quantile(y,seq(0,1,l=numGroups+1))
+  th[1]=th[1]-1
+  if(anyDuplicated(th))stop('quantiles for diff(y) not unique!')
+  as.integer(cut(y,breaks=th))
+
+}
+
+
+## a port of https://github.com/benfulcher/hctsa/blob/master/Operations/FC_Surprise.m
+## method = 'updown' and prior='dist' only
+fc_surprise_dist_upd=function(y,numGroups=2,memory=5,numIters=500){
+  yth=try(sb_coarsegrain(y,numGroups))
+  if(inherits(yth,'try-error'))return(NA)
+  N=length(yth)
+  # rs=sample(N-memory)+memory  ## my ts is not that long
+  # rs=sort(head(rs,numIters))  ## my ts is not that long
+  rs=(memory+1):length(yth)
+  store=sapply(1:length(rs),function(i)sum(yth[(rs[i]-memory):(rs[i]-1)]==yth[rs[i]])/memory)
+  store[store==0]=1
+  store=-log(store)
+  mean(store)
+}
+
+sy_trend_meanYC=function(y,n=NULL){
+  if(is.null(n)&is.ts(y)&frequency(y)>1)n=frequency(y)
+  mean(cumsum(scale(head(y,n))))
 }
